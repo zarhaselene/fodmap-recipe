cf191eb3100f6771663fe2da46b1bf0803f54b3a
Zarha <zarhaselene@gmail.com>
2025-03-25 14:01:02 +0100

    Increase max width of RecipeDetail component for improved layout

--- src/app/recipes/[id]/page.jsx
+++ src/app/recipes/[id]/page.jsx
@@ -233,7 +233,7 @@ export default function RecipeDetail() {
       variants={pageVariants}
     >
       {/* Back button */}
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-6">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-6">
         <motion.div
           whileHover={{ x: -5 }}
           transition={{ type: "spring", stiffness: 400 }}
