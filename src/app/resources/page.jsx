734dd073cebbd9ab4581644f244ec2c7cdbde695
Zarha <zarhaselene@gmail.com>
2025-03-31 17:38:52 +0200

    Add loading spinner animation to resources page

--- src/app/resources/page.jsx
+++ src/app/resources/page.jsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { Suspense } from "react";
 import Resources from "../components/resources/ResourcesPage";
+import { motion } from "framer-motion";
 
 const ResourcesPage = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center min-h-screen">
+          <motion.div
+            className="border-t-4 border-teal-500 border-solid w-8 h-8 rounded-full"
+            animate={{ rotate: 360 }}
+            transition={{
+              duration: 1,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          />
+        </div>
+      }
+    >
       <Resources />
     </Suspense>
   );
